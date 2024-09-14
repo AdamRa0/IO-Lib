634410580f1b50cad3c2ccaee4a22a6ff47491de
Adam Rao <adamokoth@protonmail.com>
2024-09-14 03:07:46 +0300

    Create print_string function

--- io_lib.asm
+++ io_lib.asm
@@ -10,6 +10,7 @@ exit:
     mov rax, 60
     syscall
 
+; Function finding string length
 string_length:
     mov rsi, rdi
     xor rbx, rbx
@@ -23,13 +24,22 @@ string_length:
     inc rbx
 
     jmp .loop
-    
 
+; Function printing string
 .done:
     mov rax, rbx
     ret
 
+print_string:
+    mov rsi, rdi ; copy test_str to rsi
+    mov rax, 1 ; Linux sys_write syscall val
+    mov rdi, 1 ; FD
+    mov rdx, 14 ; Length of test_str
+    syscall
+    ret ; return to _start
+
 _start:
     mov rdi, test_str
-    call string_length
+    ;call string_length
+    call print_string
     call exit
