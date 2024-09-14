44b573248b00552a9704bffefbaa814d2ed19480
Adam Rao <adamokoth@protonmail.com>
2024-09-14 03:11:36 +0300

    create print_newline function

--- io_lib.asm
+++ io_lib.asm
@@ -1,6 +1,6 @@
 section .data
     test_str: db 'Hello brethren', 0 ; test varible to ensure function works
-
+    new_line: db 0xA ; code for newline
 global _start
 
 section .text
@@ -30,6 +30,14 @@ string_length:
     mov rax, rbx
     ret
 
+print_newline:
+    mov rsi, new_line
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 1
+    syscall
+    ret
+
 print_string:
     mov rsi, rdi ; copy test_str to rsi
     mov rax, 1 ; Linux sys_write syscall val
@@ -42,4 +50,5 @@ _start:
     mov rdi, test_str
     ;call string_length
     call print_string
+    call print_newline
     call exit
